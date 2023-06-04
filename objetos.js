55106a657ab3002cb67b51a09c3bfd5f29b7f3dc
EricWithC04 <negativegame117@gmail.com>
2023-06-03 23:08:04 -0300

    ejercicios de objetos terminados

---
+++ objetos.js
@@ -0,0 +1,29 @@
+let producto = {
+    nombre: "zanahoria",
+    precio: 15,
+    cantidad: 12
+}
+
+function calcularTotal (product) {
+    return product.precio * product.cantidad;
+}
+
+console.log(calcularTotal(producto));
+
+let persona = {
+    nombre: "Alvaro",
+    edad: 28,
+    profesion: "oftalmologo"
+}
+
+function presentarPersona (person) {
+    console.log(`Hola, soy ${person.nombre}, tengo ${person.edad} años y soy ${person.profesion}`);
+}
+
+presentarPersona(persona);
+
+function esMayorDeEdad (person) {
+    return person.edad >= 18
+}
+
+console.log(esMayorDeEdad(persona));
